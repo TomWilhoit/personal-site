be2f91097928828820433d2b2549185337178772
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-12-08 14:35:49 -0700

    Boilerplates components for site

---
+++ src/containers/ApiExamples.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ApiExamples() {
+  return (
+    <div className="ApiExamples">
+      <header className="ApiExamples-header">
+        <text>ApiExamples</text>
+      </header>
+    </div>
+  );
+}
+
+export default ApiExamples;
---
+++ src/containers/Documents.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Documents() {
+  return (
+    <div className="Documents">
+      <header className="Documents-header">
+        <text>Documents</text>
+      </header>
+    </div>
+  );
+}
+
+export default Documents;
