f02912dfb1fa9c15d32afa0cb4981e068dfab4e7
Tom Wilhoit <tomwilhoit.business@gmail.com>
2020-01-04 17:14:57 -0700

    finished routes for React-Router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,64 +1,30 @@
 import React, { Component } from "react";
-import Home from "./Home"
+import Home from "./Home";
 import Resume from "./Resume";
 import AboutMe from "./AboutMe";
 import ApiExamples from "./ApiExamples";
 import Projects from "./Projects";
-import { Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
 import "../styles/App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentPage: "App"
+      placeHolder: true
     };
   }
 
-  changePage = string => {
-    this.setState({ currentPage: string });
-  };
-
   render() {
-    return(
-      <main className='App'>
-        <body>
-        <Route exact path='/' component={Home}/>
-        <Route path='/about_me' component={AboutMe} />
-        <Route path='/resume' component={Resume} />
-        <Route path='/projects' component={Projects} />
-        <Route path='/api_examples' component={ApiExamples} />
-        </body>
+    return (
+      <main className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/about_me" component={AboutMe} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/api_examples" component={ApiExamples} />
       </main>
-    )
-    // const { currentPage } = this.state;
-    // if (currentPage === "App") {
-    //   return (
-    //     <div className="App">
-    //       <header className="App-header">
-    //         <div>Home</div>
-    //         <button onClick={() => this.changePage("AboutMe")}>About Me</button>
-    //         <button onClick={() => this.changePage("Documents")}>
-    //           Documents
-    //         </button>
-    //         <button onClick={() => this.changePage("Projects")}>
-    //           Projects
-    //         </button>
-    //         <button onClick={() => this.changePage("ApiExamples")}>
-    //           Api Examples
-    //         </button>
-    //       </header>
-    //     </div>
-    //   );
-    // } else if (currentPage === "AboutMe") {
-    //   return <AboutMe changePage={this.changePage} />;
-    // } else if (currentPage === "Documents") {
-    //   return <Documents changePage={this.changePage} />;
-    // } else if (currentPage === "Projects") {
-    //   return <Projects changePage={this.changePage} />;
-    // } else if (currentPage === "ApiExamples") {
-    //   return <ApiExamples changePage={this.changePage} />;
-    // }
+    );
   }
 }
 
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Projects.css"
 
 export default class Projects extends Component {
   render() {
     return (
       <div>
-        I enjoy Projects
+        <div>Projects</div>
+        <Link className="page_link" to="/">Home</Link>
       </div>
-    )
+    );
   }
 }
